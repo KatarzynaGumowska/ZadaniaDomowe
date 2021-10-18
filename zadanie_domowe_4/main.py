56b540e4f2f995772a6ea0060022ecd4ea3f6b0d
KMdev <karolmularski94@gmail.com>
2021-10-18 13:02:54 +0200

    Update main.py

--- zadanie_domowe_4/main.py
+++ zadanie_domowe_4/main.py
@@ -1,17 +1,32 @@
-# Stworzyć klasę użytkownika, która będzie dziedziczyć po klasie człowiek (imię, nazwisko, etc.)
-# oraz będzie miała atrybut wieku automatycznie obliczany na podstawie daty urodzenia.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import datetime
+
 
 class Czlowiek:
 
-    def __init__(self, imie, nazwisko, data_urodzenia):
+    def __init__(self, imie, nazwisko, data_urodzenia, telefon):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.set_data_urodzenia = int(data_urodzenia)
+        self.data_urodzenia = data_urodzenia
 
+        try:
+            if len(telefon.split(" ")) == 3 or len(telefon.split("-")) == 3 or (int(telefon) and len(telefon) == 9):
+                return 
+            raise ValueError()
+        except ValueError as e:
+            print("Nieprawidłowy format telefonu: " + telefon)
+
+    @property
     def wiek(self):
-        return 2021 - self.set_data_urodzenia
+        born = datetime.datetime.strptime('11 05 2018', "%d %m %Y")
+        today = datetime.datetime.today()
+        return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
 
-Uzytkownik = Czlowiek("Kasia", "Gumowska", 1985)
-print(vars(Uzytkownik))
-print(Uzytkownik.wiek())
+if __name__ == '__main__':
+    Uzytkownik = Czlowiek("Kasia", "Gumowska", "12 05 1990", "203 403 380")
+    Uzytkownik = Czlowiek("Kasia", "Gumowska", "12 05 1990", "203-403-380")
+    Uzytkownik = Czlowiek("Kasia", "Gumowska", "12 05 1990", "203403380")
+    Uzytkownik = Czlowiek("Kasia", "Gumowska", "12 05 1990", "203/403a380")
